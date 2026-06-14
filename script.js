1f8615da9a228c73d40235719e4e79b24569269c
lukejensen994-png <lukejensen994@gmail.com>
2026-06-14 19:16:21 +0100

    Remove old button click behavior

--- script.js
+++
@@ -1,6 +0,0 @@
-const button = document.querySelector('#test-button');
-const statusMessage = document.querySelector('#status-message');
-
-button.addEventListener('click', () => {
-  statusMessage.textContent = 'This button has been pressed.';
-});
